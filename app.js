c59e9e8ef984540273c6622944fd9a40cd7cc046
JT <jerylltan@gmail.com>
2022-12-23 00:40:06 +0800

    Added Sorting logic

--- app.js
+++ app.js
@@ -1,9 +1,67 @@
-let mylibrary = [];
-const openPopUpButton = document.querySelector('[data-open-button]')
-const closePopUpButton = document.querySelector('[data-close-button]')
+let publishingDate = [];
+let insertionDate = [];
+const openPopUpButton = document.querySelector('[data-open-button-add]')
+const closePopUpButton = document.querySelector('[data-close-button-add]')
 const overlay = document.getElementById('overlay')
 const form = document.getElementById('addBook');
 const cardContainer = document.querySelector('[data-card-container]')
+const orderDate = document.getElementById("orderDate")
+let cardCounter = 0;
+
+openPopUpButton.addEventListener("click", () => {
+    const popup = document.getElementById("popup")
+    console.log(popup)
+    openPopUp(popup)
+})
+
+closePopUpButton.addEventListener("click", () => {
+    const popup = document.getElementById("popup")
+    closePopUp(popup)
+})
+
+function addByPublishing(book) {
+    const bookDate = new Date(book["Published Date"])
+
+    if (publishingDate.length === 0 || bookDate >= publishingDate[publishingDate.length-1]) {
+        publishingDate.push(book)
+    } 
+    else if (publishingDate.length === 1) {
+        if (bookDate <= new Date(publishingDate[0]["Published Date"])) publishingDate.splice(0, 0, book)
+    }
+    else {
+        let i = 0;
+        while (i < publishingDate.length-1) {
+            if(bookDate >= new Date(publishingDate[i]["Published Date"]) 
+            && bookDate <= new Date(publishingDate[i+1]["Published Date"])) {
+                publishingDate.splice(i, 0, book)
+                break
+            }
+        }
+    }
+}
+
+function removeInsertion(book, removeFrom) {
+    const title = book.title
+    const author = book.author
+    
+    for(i = 0; i < removeFrom.length; i++) {
+        if (i.title === title && i.author === author) {
+            removeFrom.splice(i, 1)
+            return
+        }
+    }
+}
+
+function Book(title, author, pages, publishDate, status) {
+
+    return {
+        "Title": title,
+        "Author": author,
+        "Total Pages": pages,
+        "Published Date": publishDate,
+        "Read Status": status
+    }
+}
 
 function addReadStatus(status) {
     const readConDiv = document.createElement("div")
@@ -35,16 +93,45 @@ function addReadStatus(status) {
     return readConDiv
 }
 
+function removeCard(cardId, book) {
+    const closeButton = document.createElement("button")
+    closeButton.classList.add("remove-card")
+    closeButton.textContent = "x"
+
+    closeButton.addEventListener("click", () => {
+        const card = document.getElementById(`card-${cardId}`)
+        cardContainer.removeChild(card)
+        removeInsertion(book, insertionDate)
+        removeInsertion(book, publishingDate)
+    })
+    return closeButton
+}
+
+function clearForm(form) {
+    Array.from(form.elements).forEach(e => {
+        if (e.type !== "submit") {
+            e.value = ""
+        }
+    })
+}
+
 function newCard(book) {
     const card = document.createElement("div")
+    
+    card.id = "card-" + cardCounter
     card.classList.add("card")
+
+    const removeButton = removeCard(cardCounter, book)
+    
+    cardCounter++
+    card.appendChild(removeButton)
+
     for (const info in book) {
         if (info !== "Read Status") {
             const para = document.createElement("p")
             para.textContent = `${info}: ${book[info]}`
             card.appendChild(para)
         } else {
-            console.log("hi")
             const status = addReadStatus(book[info])
             card.appendChild(status)
         }
@@ -55,6 +142,7 @@ function newCard(book) {
 form.addEventListener("submit", e =>{
     e.preventDefault()
     const form = document.forms.addBook
+    console.log(form.elements)
     const book = new Book(
         form.elements[0].value,
         form.elements[1].value,
@@ -62,22 +150,28 @@ form.addEventListener("submit", e =>{
         form.elements[3].value,
         form.elements[4].value, 
         )
-    mylibrary.push(book)
+    clearForm(form)
+    insertionDate.push(book)
+    addByPublishing(book)
     const card = newCard(book)
     cardContainer.appendChild(card)
-    console.log(mylibrary)
     }
 )
 
-openPopUpButton.addEventListener("click", () => {
-    const popup = document.getElementById("popup")
-    console.log(popup)
-    openPopUp(popup)
-})
-
-closePopUpButton.addEventListener("click", () => {
-    const popup = document.getElementById("popup")
-    closePopUp(popup)
+orderDate.addEventListener("change", e => {
+    let newCards = []
+    if (e.target.value === "iDate") {
+        if (insertionDate.length === 0) return
+        for(i in insertionDate) {
+            newCards.push(newCard(i))
+        }
+    } else {
+        if (publishingDate.length === 0) return
+        for(i in publishingDate) {
+            newCards.push(newCard(i))
+        }
+    }
+    cardContainer.replaceChildren(...newCards)
 })
 
 function openPopUp(popup) {
@@ -90,18 +184,4 @@ function closePopUp(popup) {
     if (popup == null) return
     popup.classList.remove("active")
     overlay.classList.remove("active")
-}
-
-function Book(title, author, pages, publishDate, status) {
-    return {
-        "Title": title,
-        "Author": author,
-        "Total Pages": pages,
-        "Published Date": publishDate,
-        "Read Status": status
-    }
-}
-
-function addBookToLibrary() {
-
 }
